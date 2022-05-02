3f7e3f6490160ae6fc6c97737c664e9c9b26c09f
xLunith <liandrei2000@gmail.com>
2022-05-02 03:10:40 -0700

    Fixed waits

--- LogHours.py
+++ LogHours.py
@@ -7,18 +7,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.firefox import GeckoDriverManager
-from datetime import datetime, timedelta, date
+from datetime import timedelta, date, time
 
+# Get user input for login and shift info
 username = input("Username: ")
 password = input("Password: ")
 dayInput = input("Enter First Date to Log (m/d/YYYY): ")
+print("Hours (0-23) : Minutes (increments of 15)")
+startInput = input("Enter Start Time (H:mm): ")
+endInput = input("Enter End Time (H:mm): ")
 
+# Parse dayInput as date object
+dayValues = dayInput.split('/')
+m = int(dayValues[0])
+d = int(dayValues[1])
+Y = int(dayValues[2])
+firstDay = date(Y, m, d)
+
+# Create dates for 8 days across 2 weeks
 format = "%m/%d/%Y"
-firstDay = date(2022, 5, 2)
-
 addDay = timedelta(days=1)
 addWeek = timedelta(days=4)
-
 dates=[]
 for i in range(2):
 	dates.append(firstDay.strftime(format))
@@ -27,39 +36,84 @@ for i in range(2):
 		dates.append(firstDay.strftime(format))
 	firstDay = firstDay + addWeek
 
-fromTime = 7
-toTime = 12
+# Parse Time inputs into formatted strings
+# and calculate hours worked
+timeValues = startInput.split(":")
+timeValues += endInput.split(":")
 
+# Only converts hours to ints
+for i in range(0,4,2):
+	timeValues[i] = int(timeValues[i])
+
+# Creates timedelta objects to subtract
+fromTime = timedelta(hours=timeValues[0], minutes=int(timeValues[1]))
+toTime = timedelta(hours=timeValues[2], minutes=int(timeValues[3]))
+hours = toTime - fromTime
+hours = hours.seconds/3600
+
+# Formats 24 to 12 hh and adds AM/PM
+if timeValues[0] > 12:
+	timeValues[0] -= 12
+	fromTime = str(timeValues[0]) + ":" + timeValues[1] + " PM"
+elif timeValues[0] == 12:
+	fromTime = str(timeValues[0]) + ":" + timeValues[1] + " PM"
+elif timeValues[0] == 0:
+	fromTime = "12:" + timeValues[1] + " AM"
+else:
+	fromTime = str(timeValues[0]) + ":" + timeValues[1] + " AM"
+
+if timeValues[2] > 12:
+	timeValues[2] -= 12
+	toTime = str(timeValues[2]) + ":" + timeValues[3] + " PM"
+elif timeValues[2] == 12:
+	toTime = str(timeValues[2]) + ":" + timeValues[3] + " PM"
+elif timeValues[2] == 0:
+	toTime = "12:" + timeValues[3] + " AM"
+else:
+	toTime = str(timeValues[2]) + ":" + timeValues[3] + " AM"
+
+# Get Firefox Webdriver automatically
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+# Create wait object
+wait = WebDriverWait(driver, 10)
+
+# Load TAS login page
 driver.get("http://tas.asu.edu")
 
-userInput = driver.find_element_by_id("username")
-passInput = driver.find_element_by_id("password")
+# Log in to ASU
+userInput = driver.find_element(By.ID, "username")
+passInput = driver.find_element(By.ID, "password")
 
 userInput.send_keys(username)
 passInput.send_keys(password)
 passInput.send_keys(Keys.RETURN)
 
 try:
-	duoFrame = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "duo_iframe")))
+	# Wait for Duo Login page to load and switch focus to duo iFrame
+	duoFrame = wait.until(EC.presence_of_element_located((By.ID, "duo_iframe")))
 	driver.switch_to.frame(duoFrame)
-	pushButton = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Send Me a Push')]")))
+
+	# Wait for push buttons to load and click
+	pushButton = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Send Me a Push')]")))
 	pushButton.click()
-	print("click")
 except TimeoutException:
 	print("Timeout")
 	exit()
 
-input("Press enter after authorization...")
+# Wait for user to authorize and load page
+input("Press enter after authorization and page load...")
 
+# Return focus to main page
 driver.switch_to.default_content()
 
+# Iterate once for each date
 for day in dates:
 	newReqBtn = driver.find_element(By.ID, "btnNewRequest")
 	newReqBtn.click()
-
-
-	typeSel = Select(WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, "cbotype"))))
+	
+	# Wait for popup window to load
+	typeSel = Select(wait.until(EC.element_to_be_clickable((By.ID, "cbotype"))))
 	typeSel.select_by_value("Hours Worked")
 
 	fromInp = driver.find_element(By.ID, "txtfrom")
@@ -67,11 +121,8 @@ for day in dates:
 	fromInp.send_keys(day)
 	fromInp.send_keys(Keys.RETURN)
 
-	WebDriverWait(driver, 2).until(EC.invisibility_of_element((By.CLASS_NAME, "ui-datepicker-title")))
-
-	hours = toTime - fromTime
-	fromTime = str(fromTime) + ":00 AM"
-	toTime = str(toTime) + ":00 PM"
+	# Wait for datepicker to vanish
+	wait.until(EC.invisibility_of_element((By.CLASS_NAME, "ui-datepicker-title")))
 
 	fromSel = Select(driver.find_element(By.ID, "txthourfrom"))
 	fromSel.select_by_value(fromTime)
@@ -84,5 +135,12 @@ for day in dates:
 	subBtn = driver.find_element(By.XPATH, "//*[contains(text(), 'Submit Request')]")
 	subBtn.click()
 
-	confBtn = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Yes, submit request')]")))
+	confBtn = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Yes, submit request')]")))
+	# Get both overlay divs
+	overlays = driver.find_elements(By.CLASS_NAME, "ui-widget-overlay ui-front")
 	confBtn.click()
+
+	# Wait for popups to close
+	for overlay in overlays:
+		wait.until(EC.invisibility_of_element(overlay))
+	
